d0b8efdcca31dc95e0b95fd4bfdebd2c9b915ce2
liawesomesaucer <vincent.oddrepublic@gmail.com>
2017-05-09 17:51:02 -0700

    Build step 1 of newmap

--- web/app/components/App.js
+++ web/app/components/App.js
@@ -8,7 +8,7 @@ const App = ({ children }) =>
             <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <a className="navbar-brand" href="/">Navbar</a>
+            <a className="navbar-brand" href="/">Guorient</a>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item active">
