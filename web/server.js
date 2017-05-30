cc91cf9c840027e645142adfd3847eec26cebc84
liawesomesaucer <vincent.oddrepublic@gmail.com>
2017-05-30 16:39:35 -0700

    Add listen for port in web server

--- web/server.js
+++ web/server.js
@@ -21,7 +21,7 @@ new WebpackDevServer(webpack(config), {
       chunks: false,
       chunkModules: false
     }
-}).listen(3000, 'localhost', function (err) {
+}).listen(process.env.PORT || 3000, 'localhost', function (err) {
     if (err) {
         console.log(err);
     }
