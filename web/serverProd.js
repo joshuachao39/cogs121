fddbd0e19017b44c6f25d3994d23e368186da025
liawesomesaucer <vincent.oddrepublic@gmail.com>
2017-05-30 20:09:05 -0700

    Fixed prod possibly

---
+++ web/serverProd.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+const path = require('path');
+const express = require('express');
+
+module.exports = {
+  app: function () {
+    const app = express()
+    const indexPath = path.join(__dirname, '/dist/index.html');
+    const publicPath = express.static(path.join(__dirname, '/dist'));
+
+    // app.use('/dist', publicPath)
+    app.use(express.static(path.join(__dirname, '/dist')));
+    app.get('/', function (_, res) { res.sendFile(indexPath) })
+    app.get('/maps', function (_, res) { res.sendFile(indexPath) })
+    app.get('/maps/:mapId', function (_, res) { res.sendFile(indexPath) })
+    app.get('/maps/New', function (_, res) { res.sendFile(indexPath) })
+
+    return app
+  }
+}
--- web/webpack.production.config.js
+++ web/webpack.production.config.js
@@ -14,8 +14,8 @@ module.exports = {
     // Where you want the output to go
     output: {
         path: path.join(__dirname, '/dist/'),
-        filename: '[name]-[hash].min.js',
-        publicPath: '/'
+        filename: 'bundle.js',
+        publicPath: '/dist/'
     },
     plugins: [
         // webpack gives your modules and chunks ids to identify them. Webpack can vary the
@@ -34,7 +34,7 @@ module.exports = {
         // extracts the css from the js files and puts them on a separate .css file. this is for
         // performance and is used in prod environments. Styles load faster on their own .css
         // file as they dont have to wait for the JS to load.
-        new ExtractTextPlugin('[name]-[hash].min.css'),
+        new ExtractTextPlugin('styles.min.css'),
         // handles uglifying js
         new webpack.optimize.UglifyJsPlugin({
             compressor: {
@@ -78,11 +78,20 @@ module.exports = {
             test: /\.json?$/,
             loader: 'json'
         }, {
-            test: /\.scss$/,
+            test: /\.(scss|css)$/,
             // we extract the styles into their own .css file instead of having
             // them inside the js.
-            loader: ExtractTextPlugin.extract('style', 'css?modules&localIdentName=[name]---[local]---[hash:base64:5]!sass')
+            // loader: ExtractTextPlugin.extract('style', 'css?modules&localIdentName=[name]---[local]---[hash:base64:5]!sass'),
+            include: [
+                path.resolve(__dirname, 'node_modules'),
+                path.resolve(__dirname, 'app/styles') //<== This solved the issue
+            ],
+            loader: 'style-loader!css-loader!sass-loader',
+            // include: path.join(__dirname, 'dist', 'styles')
         }, {
+            test: /\.png$/,
+            loader: 'file'
+        },{
             test: /\.woff(2)?(\?[a-z0-9#=&.]+)?$/,
             loader: 'url?limit=10000&mimetype=application/font-woff'
         }, {
